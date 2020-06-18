3b116a6c6b63684ea3e79a61574686d654a8fff2
chinan <269085923@qq.com>
2020-06-18 07:26:37 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,7 +14,6 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        dd($user); die;
         return view('users.show', compact('user'));
     }
 
@@ -25,6 +24,14 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程');
+        return redirect()->route('users.show', [$user]);
     }
 }
